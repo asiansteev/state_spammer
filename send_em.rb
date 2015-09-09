6e1b61c608421cac6ca4d6788fcb06c7ba5b90fa
Steven Hayes <asiansteev@gmail.com>
2015-09-09 17:21:00 -0400

    old style mechanize

--- send_em.rb
+++ send_em.rb
@@ -12,17 +12,29 @@ require 'yaml'
 @state = ''
 @zip = ''
 @country = 'United States'
+@phone = ''
 
-state = YAML.load_file('yml/arizona.yml')
+state = YAML.load_file('yml/florida.yml')
 agent = Mechanize.new
 
 @state = 'KY' if state['state_form'] == 'abbr'
+@state = 'KY - Kentucky' if state['state_form'] == 'abbr - full'
+@state = 'KENTUCKY' if state['state_form'] == 'all caps'
 @country = 'US' if state['country_form'] == 'abbr'
+@country = 'U.S.A.' if state['country_form'] == 'U.S.A.'
+@country = 'UNITED STATES' if state['country_form'] == 'all caps'
+@country = 'USA' if state['country_form'] == 'USA'
 
 page = agent.get(state['url'])
-form = page.form_with(id: state['form_id'])
+form = page.form_with(state['form_by'].to_sym => state['form'])
 state['attrs'].each do |attr|
   form[attr.last] = instance_variable_get("@#{attr.first}")
+end
+
+if state['other']
+  state['other'].each do |attr|
+    form[attr.first] = attr.last
+  end
 end
 
 if state['check']
@@ -31,7 +43,13 @@ if state['check']
   end
 end
 
-#page = agent.submit(form)
-#pp form
+if state['uncheck']
+  state['uncheck'].split(',').each do |uncheck|
+    form.checkbox_with(name: check).selected = false
+  end
+end
+
+page = agent.submit(form)
+pp form
 success = page.body.include? state['success']
-p "Success: #{success}" 
+p "Success: #{success}"
